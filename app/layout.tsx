62fdb9cb2793332c65a48d0d9b0861c9ca3ceeed
Lucas <lucas@brandora.app>
2025-12-10 17:06:12 +0200

    Fix Facebook Pixel ID and remove redundant initialization

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '826011514643377');
+            fbq('init', '1222009323075471');
             fbq('track', 'PageView');
           `}
         </Script>
@@ -47,7 +47,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=826011514643377&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1222009323075471&ev=PageView&noscript=1"
           />
         </noscript>
         {children}
